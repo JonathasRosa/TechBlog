c4abbe45d65974c1b8e31e468d449cf7db3596ea
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-04-20 11:08:44 -0300

    Update TechBlog-Angular

--- the-blog/src/app/app.module.ts
+++ the-blog/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
@@ -18,7 +17,6 @@ import { HomeComponent } from './pages/home/home.component';
     MenuTitleComponent,
     BigCardComponent,
     SmallCardComponent,
-    FooterComponent,
     HomeComponent
   ],
   imports: [
