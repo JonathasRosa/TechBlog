41588f958da103f487230f69a8f5c8dbb0d4f5bc
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-04-20 15:49:15 -0300

    Update TechBlob

--- the-blog/src/app/pages/content/content.component.ts
+++ the-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  
+  @Input()
+  photoCover: string = "../../../assets/img/angular-2.png"
+  @Input()
+  cardTitle: string = "Um overview sobre o framework Angular"
+  @Input()
+  cardDescription: string = "Quando decidimos utilizar uma tecnologia em um novo projeto, é natural que uma das primeiras coisa que façamos seja entender o propósito que essa tecnologia tem a oferecer e como podemos ter um maior aproveitamento de suas funcionalidades e características. Para adquirir este entendimento é necessário ter uma visão geral da arquitetura dessa tecnologia. Pensando nisso, vamos demonstrar neste artigo uma visão geral da arquitetura do Angular e, assim, possibilitar aos leitores a oportunidade de entender melhor como funciona este fantástico framework."
   constructor() { }
   ngOnInit(): void { }
 }
