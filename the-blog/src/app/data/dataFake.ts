d27e558be5c4a9c52ad7b6a276eca1fb88d547dc
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-04-22 09:35:09 -0300

    Update Tech-Blog

---
+++ the-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": 0,
+        "photo": "../../../assets/img/angular-2.png",
+        "title": "Ângular",
+        "description": "Um overview sobre o framework Angular"
+    }, 
+    {
+        "id": 1,
+        "photo": "../../../assets/img/programacao-1.png",
+        "title": "O que é Programação?",
+        "description": "Um overview sobre o framework Angular"
+    }, 
+    {
+        "id": 2,
+        "photo": "../../../assets/img/programacao-2.png",
+        "title": "Por onde começar na programação?",
+        "description": "Por onde e como estudarperogramação"
+    }, 
+    {
+        "id": 3,
+        "photo": "../../../assets/img/linguagens.png",
+        "title": "Qual linguagem estudar?",
+        "description": "Qual é a melhor linguagem para iniciar os estudos em programação"
+    }
+]
